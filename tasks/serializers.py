4f6f364f9a61b8718599da05335a24bf58128c4c
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-18 13:40:37 +0530

    added task serializer

---
+++ tasks/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields = ('user')
+# The TaskSerializer is a ModelSerializer that automatically generates fields based on the Task model.
+# The Meta class specifies the model to be serialized and the fields to include in the serialization.
+# fields = '__all__' indicates that all fields of the Task model should be included in the serialization.
+
