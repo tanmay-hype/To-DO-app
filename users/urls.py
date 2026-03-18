c78c5f3dddf322c4bb9870141d7b94d45be90a1f
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-18 17:23:08 +0530

    added JWT token endpoints and removed session based authentications

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #TokenObtainPairView and TokenRefreshView are imported for handling JWT authentication
 from .views import RegisterView, LoginView, LogoutView , UserListCreateView, UserDetailView #RegisterView, LoginView and LogoutView are imported from the views module to be used in the URL patterns
 #RegisterView, LoginView and LogoutView are imported 
 # from the views module to be used in the URL patterns
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),#The URL pattern for user registration is defined, which maps to the RegisterView class-based view.
-    path('login/', LoginView.as_view(), name='login'),#The URL pattern for user login is defined, which maps to the LoginView class-based view.
-    path('logout/', LogoutView.as_view(), name='logout'),#The URL pattern for user logout is defined, which maps to the LogoutView class-based view.
+  
+   # path('login/', LoginView.as_view(), name='login'),#The URL pattern for user login is defined, which maps to the LoginView class-based view.
+   # path('logout/', LogoutView.as_view(), name='logout'),#The URL pattern for user logout is defined, which maps to the LogoutView class-based view.
+    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   
     path('', UserListCreateView.as_view(), name='user-list-create'),#The URL pattern for listing and creating users is defined, which maps to the UserListCreateView class-based view.
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),#The URL pattern for user detail view is defined, which includes a dynamic segment <int:pk> to capture the primary key of the user. This allows for retrieving, updating, or deleting a specific user based on its unique identifier.]
 ]
