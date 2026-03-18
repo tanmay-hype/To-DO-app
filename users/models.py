6613e38c1f5e764ecf9358312aadc034a527afde
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-18 12:02:37 +0530

    custom user model created and migrated

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+# Create your custom user model by extending AbstractUser
+
+class User(AbstractUser):
+    # Add any additional fields you need for your custom user model
+    pass
+
+
