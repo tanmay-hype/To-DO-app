9fcafa0db128c7f223d696e1d36bc424113369af
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-18 16:42:10 +0530

    added user crud routes

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),#The URL pattern for user login is defined, which maps to the LoginView class-based view.
     path('logout/', LogoutView.as_view(), name='logout'),#The URL pattern for user logout is defined, which maps to the LogoutView class-based view.
     path('', UserListCreateView.as_view(), name='user-list-create'),#The URL pattern for listing and creating users is defined, which maps to the UserListCreateView class-based view.
-    path('/<int:pk>/', UserDetailView.as_view(), name='user-detail'),#The URL pattern for user detail view is defined, which includes a dynamic segment <int:pk> to capture the primary key of the user. This allows for retrieving, updating, or deleting a specific user based on its unique identifier.]
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),#The URL pattern for user detail view is defined, which includes a dynamic segment <int:pk> to capture the primary key of the user. This allows for retrieving, updating, or deleting a specific user based on its unique identifier.]
 ]
