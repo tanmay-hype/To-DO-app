7b37e61f3096b902263a8cf5f3057e69f2c3131f
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-19 15:13:24 +0530

    added category routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import TaskListCreateView, TaskDetailView
+from .views import TaskListCreateView, TaskDetailView, CategoryListCreateView, CategoryDetailView   
+
 
 urlpatterns = [
     path('', TaskListCreateView.as_view(), name='task-list-create'),#The URL
     path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),#The URL pattern for task detail view is defined, which includes a dynamic segment <int:pk> to capture the primary key of the task. This allows for retrieving, updating, or deleting a specific task based on its unique identifier.
+
+
+    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'), 
+    
 ]
+
