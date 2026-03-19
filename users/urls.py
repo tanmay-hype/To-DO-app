82bf4707c59d82a6a78708fa5b05198417577eff
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-19 11:29:23 +0530

    connected custom JWT token view to URLs

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #TokenObtainPairView and TokenRefreshView are imported for handling JWT authentication
 from .views import RegisterView, LoginView, LogoutView , UserListCreateView, UserDetailView #RegisterView, LoginView and LogoutView are imported from the views module to be used in the URL patterns
+from .views import CustomTokenView #CustomTokenView is imported for handling JWT token generation with custom claims
 #RegisterView, LoginView and LogoutView are imported 
 # from the views module to be used in the URL patterns
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),#The URL pattern for user registration is defined, which maps to the RegisterView class-based view.
-  
+    path('token/', CustomTokenView.as_view(), name='custom_token'),
+   
    # path('login/', LoginView.as_view(), name='login'),#The URL pattern for user login is defined, which maps to the LoginView class-based view.
    # path('logout/', LogoutView.as_view(), name='logout'),#The URL pattern for user logout is defined, which maps to the LogoutView class-based view.
     
