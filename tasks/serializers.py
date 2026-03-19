1a083a2bc1e15278692dad3dec52fe7d5ab08abc
Tanmay Kushwaha <tanmayproject22@gmail.com>
2026-03-19 14:40:09 +0530

    added category CRUD APIs

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Category, Task
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = ['user']
+
+# The TaskSerializer is a ModelSerializer that automatically generates fields based on the Task model.
+# The Meta class specifies the model to be serialized and the fields to include in the serialization.
+# fields = '__all__' indicates that all fields of the Task model should be included in the serialization.
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
         read_only_fields = ['user']
-# The TaskSerializer is a ModelSerializer that automatically generates fields based on the Task model.
-# The Meta class specifies the model to be serialized and the fields to include in the serialization.
-# fields = '__all__' indicates that all fields of the Task model should be included in the serialization.
-
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, Category
+from .serializers import TaskSerializer, CategorySerializer
 
 #generics is imported for creating generic class-based views
 #permissions is imported for handling permissions and access control
@@ -13,7 +13,6 @@ from .serializers import TaskSerializer
 # Create your views here.
 
 class TaskListCreateView(generics.ListCreateAPIView):#TaskListCreateView is a class-based view that handles listing and creating tasks
-    #queryset = Task.objects.all()#The queryset attribute is defined to specify the set of data that will be used for this view. In this case, it retrieves all Task objects from the database.
     serializer_class = TaskSerializer#The serializer_class attribute is defined to specify the serializer that will be used for this view. In this case, it uses the TaskSerializer to serialize and deserialize Task instances.
     permission_classes = [permissions.IsAuthenticated]#The permission_classes attribute is defined to specify the permissions required to access this view. In this case, it requires the user to be authenticated to access the view.
     
@@ -22,12 +21,8 @@ class TaskListCreateView(generics.ListCreateAPIView):#TaskListCreateView is a cl
             return Task.objects.all()
         return Task.objects.filter(user=self.request.user)
     
-   # def perform_create(self, serializer):
-       # serializer.save(user=self.request.user)#The perform_create method is overridden to associate the created task with the authenticated user. When a new task is created, it automatically sets the user field of the task to the currently authenticated user, ensuring that tasks are linked to the correct user in the database.
-
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):#TaskDetailView is a class-based view that handles retrieving, updating, and deleting individual tasks
-    #queryset = Task.objects.all()#The queryset attribute is defined to specify the set of data that will be used for this view. In this case, it retrieves all Task objects from the database.
     serializer_class = TaskSerializer#The serializer_class attribute is defined to specify the serializer that will be used for this view. In this case, it uses the TaskSerializer to serialize and deserialize Task instances.
     permission_classes = [permissions.IsAuthenticated]#The permission_classes attribute is defined to specify the permissions required to access this view. In this case, it requires the user to be authenticated to access the view.
 
@@ -35,4 +30,26 @@ class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):#TaskDetailView is a
         if self.request.user.is_staff:
             return Task.objects.all()
         return Task.objects.filter(user=self.request.user)#The get_queryset method is overridden to filter the queryset based on the authenticated user. It retrieves only the tasks that belong to the currently authenticated user, ensuring that users can only access their own tasks.
+    
+class CategoryListCreateView(generics.ListCreateAPIView):#CategoryListCreateView is a class-based view that handles listing and creating categories
+    serializer_class = CategorySerializer#The serializer_class attribute is defined to specify the serializer that will be used for this view. In this case, it uses the CategorySerializer to serialize and deserialize Category instances.
+    permission_classes = [permissions.IsAuthenticated]#The permission_classes attribute is defined to specify the permissions required to access this view. In this case, it requires the user to be authenticated to access the view.
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Category.objects.all()
+        return Category.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):#CategoryDetailView is a class-based view that handles retrieving, updating, and deleting individual categories
+    serializer_class = CategorySerializer#The serializer_class attribute is defined to specify the serializer that will be used for this view. In this case, it uses the CategorySerializer to serialize and deserialize Category instances.
+    permission_classes = [permissions.IsAuthenticated]#The permission_classes attribute is defined to specify the permissions required to access this view. In this case, it requires the user to be authenticated to access the view.
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Category.objects.all()
+        return Category.objects.filter(user=self.request.user)#The get_queryset method is overridden to filter the queryset based on the authenticated user. It retrieves only the categories that belong to the currently authenticated user, ensuring that users can only access their own categories.
+    
     
